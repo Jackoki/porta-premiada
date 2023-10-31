7558f99db430ad4e341ea5ccbc00d0bf6eb12ca9
Gabriel Kenji Inoue <kenji.inoue@hotmail.com>
2023-10-31 15:31:22 -0300

    Atualização final do projeto React Porta Premiada

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PortaModel from "../model/porta"
+import Presente from "./Presente"
 import styles from "../styles/Porta.module.css"
 
 interface PortaProps {
@@ -27,7 +28,9 @@ export default function Porta(props: PortaProps){
     return (
         <div className={styles.area} onClick={alternarSelecao}>
             <div className={ `${styles.estrutura} ${selecionada}` }>
-            {porta.aberta ? false : renderizarPorta()}
+                {porta.fechada ? 
+                    renderizarPorta() : 
+                    porta.temPresente ? <Presente /> : false}
             </div>
             <div className={styles.chao}></div>
         </div>
